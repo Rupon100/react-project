3c1388c2841a4c9021766ff39e315e61c283642d
Rupon <fardiidlamrupon@gmail.com>
2024-07-08 10:07:17 +0600

    style added

--- accordian/src/App.jsx
+++ accordian/src/App.jsx
@@ -9,19 +9,19 @@ const App = () => {
   }
   return (
     <div className="h-screen flex justify-center items-center">
-      <div className="border-2 border-sky-950 p-6 rounded">
+      <div className="max-w-[550px] border-2 border-sky-950 p-6 rounded">
           {
             data && data.length > 0 ?
               
               data.map(dataItem => (
-                <div key={dataItem.id}>
-                  <div onClick={() => toggle(dataItem.id)}>
+                <div key={dataItem.id} className='bg-sky-900 rounded m-2 p-2 text-white w-[400px]'>
+                  <div onClick={() => toggle(dataItem.id)} className='flex justify-between cursor-pointer '>
                     <h3>{dataItem.question}</h3>
                     <span>{selected == dataItem.id ? '-' : '+'}</span>
                   </div>
                   {
                     selected === dataItem.id ? (
-                      <div>{dataItem.answer}</div>
+                      <div className='text-sm  transition-all '>{dataItem.answer}</div>
                     ) : null
                   }
                 </div>
